0d789ef79dee9a52aae55b106af3620a5b8b51d6
gsdata <wanghuan@gsdata.cn>
2018-01-08 10:44:02 +0000

    update test

--- test_pdf.php
+++ test_pdf.php
@@ -3,14 +3,11 @@ if(!extension_loaded('phpwkhtmltox')) {
 	dl('phpwkhtmltox.' . PHP_SHLIB_SUFFIX);
 }
 $i = 0;
-foreach (range(1, 4) as $i) {
-    $ret = wkhtmltox_convert('pdf',
-        array('out' => '/tmp/test'.$i.'.pdf', 'imageQuality' => '95'), // global settings
+$ret = wkhtmltox_convert('pdf',
+        array('out' => '/tmp/test.pdf', 'imageQuality' => '100','pageSize'=>'A4','test'=>true), // global settings
         array(
-            array('page' => 'http://www.sina.com/'),
+            array('page' => 'http://www.sina.com/','jsdelay'=>100),
             array('page' => 'http://www.baidu.com/')
             )); // object settings
-    var_dump($ret);
-}
-
+var_dump($ret);
 ?>
