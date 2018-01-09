5a979f386f24e06034031e91ad001fed87220b33
gsdata <wanghuan@gsdata.cn>
2018-01-09 02:12:55 +0000

    update test_pdf.php

--- test_pdf.php
+++ test_pdf.php
@@ -2,11 +2,10 @@
 if(!extension_loaded('phpwkhtmltox')) {
 	dl('phpwkhtmltox.' . PHP_SHLIB_SUFFIX);
 }
-$i = 0;
 $ret = wkhtmltox_convert('pdf',
         array('out' => '/tmp/test.pdf', 'imageQuality' => '100','pageSize'=>'A4','test'=>true), // global settings
         array(
-            array('page' => 'http://www.sina.com/','jsdelay'=>100),
+            //array('page' => 'http://www.sina.com/','jsdelay'=>100),
             array('page' => 'http://www.baidu.com/')
             )); // object settings
 var_dump($ret);
